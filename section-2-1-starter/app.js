5fdb3fa0b45fe49d84e5c2f9e5278a7c4de1de98
Michael <mmin@ddmsinc.com>
2022-05-18 16:27:43 -0500

    updated starter code

--- section-2-1-starter/app.js
+++ section-2-1-starter/app.js
@@ -1,1 +1,17 @@
-Vue.createApp({}).mount('#app')
+Vue.createApp({
+    data() {
+        return {
+            firstName: 'Ahri',
+            lastName: 'Jinx'
+        }
+    }
+}).mount('#app')
+
+// Vue.createApp({
+//     data() {
+//         return {
+//             firstName: 'Jhin',
+//             lastName: 'Ashe'
+//         }
+//     }
+// }).mount('#app2')
