f0701f2b107afb2265bed784201639efbef3840d
Michael <mmin@ddmsinc.com>
2022-05-19 09:35:33 -0500

    methods property

--- section-2-1-starter/app.js
+++ section-2-1-starter/app.js
@@ -1,11 +1,20 @@
-Vue.createApp({
+const vm = Vue.createApp({
     data() {
         return {
             firstName: 'Ahri',
             lastName: 'Jinx'
         }
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${ this.lastName.toUpperCase()}`
+        }
     }
 }).mount('#app')
+
+// setTimeout(() => {
+//     vm.firstName = 'Akali'
+// }, 2000)
 
 // Vue.createApp({
 //     data() {
